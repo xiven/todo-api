a0512eceffc56f11520cd49ec80bfb6bf39f6e7e
JONATHAN <josterman@jonathans-imac.attlocal.net>
2018-03-07 09:11:17 -0500

    add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,27 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // deleteMany
+    // db.collection('Users').deleteMany({name: 'Jonathan'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // deleteOne
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // findOneAndDelete
+    db.collection('Users').findOneAndDelete({_id: new ObjectID('5a9ff1e997410e1931bf6f6c')}).then((result) => {
+        console.log(result);
+    });
+    
+    //client.close();
+});
