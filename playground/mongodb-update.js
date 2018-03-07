e36ab13d1130d1cedb460a6003c874e2e3e0c12d
JONATHAN <josterman@jonathans-imac.attlocal.net>
2018-03-07 09:52:43 -0500

    add update commands

---
+++ playground/mongodb-update.js
@@ -0,0 +1,28 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    //findOneAndUpdate
+    db.collection('Users').findOneAndUpdate(
+        {_id: new ObjectID("5a9ff0ee97410e1931bf6f08")}, 
+        {
+            $set: { 
+                name: 'Jonathan'
+            },
+            $inc: { 
+                age: 1 
+            }
+        },
+        {returnOriginal: false}
+    ).then((result) => {
+        console.log(result);
+    });
+    
+    //client.close();
+});
